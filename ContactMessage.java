884a91989ed2367703f30afba7c9e9329796d0a1
gaurijoge583 <23070992@ycce.in>
2025-12-05 17:21:31 +0530

    Create ContactMessage.java

---
+++ ContactMessage.java
@@ -0,0 +1,11 @@
+package com.portfolio.model;
+
+public class ContactMessage {
+    private String name;
+    private String email;
+    private String message;
+
+    public String getName() { return name; }
+    public String getEmail() { return email; }
+    public String getMessage() { return message; }
+}
